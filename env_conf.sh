4e0ba6479084dc2e8047c47c6fdcc37fddbf0624
yejing <yejing.ncepu@outlook.com>
2024-04-15 01:00:57 +0800

    add config vim

--- env_conf.sh
+++ env_conf.sh
@@ -7,6 +7,13 @@ for ((i=0; i<${#alias[@]}; i++)); do
     sed -i "2i\\${alias[i]}" ~/.bashrc
 done
 
+# config vim
+if [ ! -f ~/.vimrc ]; then
+    touch ~/.vimrc # 创建文件
+fi
+sed -i "1i\\:set nu" ~/.vimrc # -i: 表示直接修改文件而不是输出到终端 1i: 表示在第1行进行插入操作  \\进行转义
+
+
 # add common packages
 apt update
 apt install -y lrzsz
