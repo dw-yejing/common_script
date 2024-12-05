60030e70848f5fe8e49b7f5969138bfc40821717
yejing <yejing.ncepu@outlook.com>
2024-12-05 20:34:20 +0800

    pref powershell

--- windows/init_powershell.bat
+++ windows/init_powershell.bat
@@ -29,4 +29,9 @@ Set-Alias wc do_wc
 Set-Alias grep Select-String
 
 Set-Alias ifconfig Get-NetIPAddress
+
+function do_proxy{
+  Start-Process ms-settings:network-proxy
+}
+Set-Alias proxy do_proxy
 " >> $profile
