becce05244087508fee71f8d203b4204ad596c7b
yejing <yejing.ncepu@outlook.com>
2024-12-09 22:16:49 +0800

    update gproxy

--- windows/init_powershell.bat
+++ windows/init_powershell.bat
@@ -34,4 +34,57 @@ function do_proxy{
   Start-Process ms-settings:network-proxy
 }
 Set-Alias proxy do_proxy
+
+function do_gproxy {
+    param( [string]$state ) 
+    if ($state -eq "--off") {
+        git config --global --unset http.proxy
+        git config --global --unset https.proxy
+        echo "proxy off"
+    }
+    elseif ($state -eq "--on")  {
+        git config --global http.proxy 127.0.0.1:7890
+        git config --global https.proxy 127.0.0.1:7890
+        echo "proxy on"
+    }else{
+    echo "To enable gproxy, the '--on' flag is required. The '--off' flag is used for disabling it."
+  }
+}
+Set-Alias gproxy do_gproxy
+
+function do_dmirror{
+    param([string]$pattern)
+    if ($pattern -eq "--pip") {
+        echo "-i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
+    }
+    elseif ($pattern -eq "--mvn")  {
+        echo "<mirror> 
+  <id>alimaven2</id> 
+  <mirrorOf>central</mirrorOf> 
+  <name>aliyun maven</name> 
+  <url>http://maven.aliyun.com/nexus/content/repositories/central/</url> 
+</mirror>"
+    }elseif ($pattern -eq "--npm")  {
+        echo "--registry=https://registry.npmmirror.com"
+    }
+  elseif ($pattern -eq "--help")  {
+        echo "to list the pip mirrors the '--pip' flag is required, the flag '--mvn' is used for listing mvn mirrors, and the flag '--npm' flag is for listing npm mirrors"
+    }
+    else{
+    echo "pip mirror:"
+        echo "-i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
+    echo ""
+        echo "mvn mirror:"
+        echo "<mirror> 
+  <id>alimaven2</id> 
+  <mirrorOf>central</mirrorOf> 
+  <name>aliyun maven</name> 
+  <url>http://maven.aliyun.com/nexus/content/repositories/central/</url> 
+</mirror>"
+    echo ""
+        echo "npm mirror:"
+        echo "--registry=https://registry.npmmirror.com"
+  }
+}
+Set-Alias dmirror do_dmirror
 " >> $profile
